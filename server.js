c1aa74ac38ee6164e27c1a83268344f7c04804fe
Konrad Brożyna <kondzio3002@gmail.com>
2023-03-05 16:35:35 +0100

    Add test department api

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const productsRoutes = require('./routes/products.routes');
 
 const app = express();
 
+const NODE_ENV = process.env.NODE_ENV;
+let dbUri = '';
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -16,11 +19,16 @@ app.use('/api', employeesRoutes);
 app.use('/api', departmentsRoutes);
 app.use('/api', productsRoutes);
 
+if(NODE_ENV === 'production') dbUri = 'url to remote db';
+else if (NODE_ENV === 'test') dbUri = 'mongodb://localhost:27017/companyDBtest'
+
+else dbUri = 'mongodb://localhost:27017/companyDB';
+
 app.use((req, res) => {
   res.status(404).send({ message: 'Not found...' });
 });
 
-mongoose.connect('mongodb://localhost:27017/companyDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
 db.once('open', () => {
@@ -28,6 +36,8 @@ db.once('open', () => {
 });
 db.on('error', err => console.log('Error ' + err));
 
-app.listen('8000', () => {
+const server = app.listen('8000', () => {
   console.log('Server is running on port: 8000');
 });
+
+module.exports = server;
